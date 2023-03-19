bf319775e27c192ebb83090724d96ed0073a3a5b
Aleksa <fankerman11@gmail.com>
2023-03-19 13:04:01 +0100

    Added Tips for using Two Pointers Algorithm.

--- Algorithms/Two Pointers/1. Two Pointers.cpp
+++ Algorithms/Two Pointers/1. Two Pointers.cpp
@@ -64,3 +64,26 @@ int main() {
     }
     return 0;
 };
+
+/*
+- When to use Two Pointers Algorithm: 
+  1. When you need to find a subarray or substring that meets certain conditions, 
+     such as a subarray that adds up to a given sum or a substring that contains all the elements of a given set.
+
+  2. When you need to search for a target value in a sorted array or string, and you can't use binary search because the elements are not unique.
+
+  3. When you need to merge two sorted arrays or strings into a single sorted array or string.
+  
+  4. When you need to solve a problem that involves comparing two or more elements of an array or string, 
+     such as finding the longest palindromic substring or the maximum sum of non-overlapping intervals.
+
+  5. When you need to identify a pattern or sequence in an array or string, 
+     such as identifying all the pairs of elements that add up to a given value or all the triplets that satisfy a certain condition.
+
+- In general, the Two Pointers algorithm is most useful when the input data is sorted or partially sorted, 
+  since this allows the algorithm to efficiently navigate the data using two pointers. 
+  However, even if the data is not sorted, the Two Pointers algorithm can still be useful in certain situations, 
+  such as when you need to find a subarray that adds up to a given sum or a substring that contains all the elements of a given set.
+
+- Overall, the Two Pointers algorithm is a versatile and powerful technique that can be used to solve a wide variety of problems efficiently and effectively.
+*/
