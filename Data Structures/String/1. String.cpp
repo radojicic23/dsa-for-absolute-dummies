c8d72bfd85dbd42bf7f2d929076b2283fc0675ac
Aleksa <fankerman11@gmail.com>
2023-03-21 11:21:20 +0100

    Added Code Explanation for Strings.

--- Data Structures/String/1. String.cpp
+++ Data Structures/String/1. String.cpp
@@ -18,3 +18,24 @@
   Strings can be manipulated in a variety of ways using built-in string functions, such as length(), substr(), find(), and replace(). 
   There are also operators that can be used with strings, such as the + operator, which concatenates two strings.
 */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  // declare and initialize a string variable
+  string myString = "Hello World!";
+  // built-in function for string size
+  int strSize = myString.length();
+  // we can use operators like "+" on strings
+  string str1 = "John";
+  string str2 = "Wick";
+  string fullName = str1 + " " + str2;
+
+  cout << myString << endl;
+  cout << strSize << endl;
+  cout << "Hello, " << fullName << "!" << endl;
+  return 0; 
+}
