d4bf901de13beccd39b53cd6a35b1adacbcb0aff
Aleksa <fankerman11@gmail.com>
2023-03-21 11:39:50 +0100

    Added Time and Space complexity explanations to Strings DS.

--- Data Structures/String/1. String.cpp
+++ Data Structures/String/1. String.cpp
@@ -53,4 +53,15 @@ int main() {
 
   These are just a few examples of the many functions available for working with strings in C++. 
   For a full list of functions, see the C++ documentation.
+
+
+- Time and Space Complexity:
+
+  1. length(): O(1) time complexity. Space complexity is also O(1), as no additional memory is required.
+  2. substr(): O(m) time complexity, where m is the length of the substring being extracted. Space complexity is O(m), as a new string of length m must be created to store the substring.
+  3. find(): O(n*m) time complexity in the worst case, where n is the length of the string and m is the length of the substring being searched for. Space complexity is O(1), as no additional memory is required.
+  4. replace(): O(m) time complexity, where m is the length of the substring being replaced. Space complexity is also O(m), as a new string of length m may need to be created to store the replacement substring.
+  5. at(): O(1) time complexity. Space complexity is also O(1), as no additional memory is required.
+  6. append(): O(m) time complexity, where m is the length of the string being appended. Space complexity is also O(m), as the string may need to allocate additional memory to accommodate the appended characters.
+  7. c_str(): O(1) time complexity. Space complexity is O(n), where n is the length of the string, as a null-terminated character array of size n+1 needs to be created to store the string.
 */
