acf0acdabb46fb90db7da679d0f93cd6aa9fe6a2
Aleksa <fankerman11@gmail.com>
2023-03-21 11:08:30 +0100

    Added String DS and Explanation for it.

---
+++ Data Structures/String/1. String.cpp
@@ -0,0 +1,20 @@
+/*
+  /$$$$$$   /$$               /$$                    
+ /$$__  $$ | $$              |__/                    
+| $$  \__//$$$$$$    /$$$$$$  /$$ /$$$$$$$   /$$$$$$ 
+|  $$$$$$|_  $$_/   /$$__  $$| $$| $$__  $$ /$$__  $$
+ \____  $$ | $$    | $$  \__/| $$| $$  \ $$| $$  \ $$
+ /$$  \ $$ | $$ /$$| $$      | $$| $$  | $$| $$  | $$
+|  $$$$$$/ |  $$$$/| $$      | $$| $$  | $$|  $$$$$$$
+ \______/   \___/  |__/      |__/|__/  |__/ \____  $$
+                                            /$$  \ $$
+                                           |  $$$$$$/
+                                            \______/ 
+*/
+
+/*
+- String is a sequence of characters stored in contiguous memory locations. 
+  It is represented by the std::string class, which is a part of the standard library.
+  Strings can be manipulated in a variety of ways using built-in string functions, such as length(), substr(), find(), and replace(). 
+  There are also operators that can be used with strings, such as the + operator, which concatenates two strings.
+*/
