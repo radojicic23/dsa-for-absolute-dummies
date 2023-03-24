29822ce630de813447da110d06f1402df3d5c7a5
Aleksa <fankerman11@gmail.com>
2023-03-24 15:31:31 +0100

    Added explanations for built-in functions and Time and Space complexity.

--- Data Structures/HashSet/1. HashSet.cpp
+++ Data Structures/HashSet/1. HashSet.cpp
@@ -56,3 +56,33 @@ int main() {
     
     return 0;
 }
+
+/*
+- HERE ARE SOME OF THE MOST COMMONLY USED BUILT-IN FUNCTIONS FOR HASHSET IN C++:
+
+  - insert(): This function is used to insert an element into the HashSet. 
+    It returns a pair object containing an iterator to the inserted element and a boolean value indicating whether the insertion was successful.
+    - The time complexity of insert() is amortized constant O(1) on average. 
+      The space complexity of insert() is O(N), where N is the number of elements in the HashSet.
+
+  - erase(): This function is used to remove an element from the HashSet. 
+    It takes an iterator or a value as an argument and returns the number of elements removed.
+    - The time complexity of erase() is constant O(1) on average. 
+      The space complexity of erase() is O(N), where N is the number of elements in the HashSet.
+
+  - clear(): This function is used to remove all elements from the HashSet.
+    - The time complexity of clear() is linear O(N), where N is the number of elements in the HashSet. The space complexity of clear() is O(1).
+
+  - find(): This function is used to find an element in the HashSet. 
+    It returns an iterator to the element if it is found, or the end() iterator if it is not found.
+    -  The time complexity of find() is constant O(1) on average. The space complexity of find() is O(1).
+
+  - size(): This function returns the number of elements in the HashSet.
+    - The time complexity of size() is constant O(1) on average. The space complexity of size() is O(1).
+
+  - empty(): This function returns a boolean value indicating whether the HashSet is empty or not.
+    - The time complexity of empty() is constant O(1) on average. The space complexity of empty() is O(1).
+
+  - count(): This function is used to count the number of elements in the HashSet that match a specific value. It returns the number of elements that match.
+    - The time complexity of count() is constant O(1) on average. The space complexity of count() is O(1).
+*/
