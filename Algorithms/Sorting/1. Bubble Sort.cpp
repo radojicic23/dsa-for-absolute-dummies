703d8a589baf5423b3ba609579eccaeb35b29c08
Aleksa <fankerman11@gmail.com>
2023-03-25 15:06:57 +0100

    Added code explanation for Bubble Sort Algorithm.

--- Algorithms/Sorting/1. Bubble Sort.cpp
+++ Algorithms/Sorting/1. Bubble Sort.cpp
@@ -15,7 +15,7 @@
 
 - Time and Space Complexity: 
   Bubble Sort has a worst-case and average-case time complexity of O(n^2), where n is the number of elements in the array. 
-  
+
   - Best-case time complexity: In the best case, where the input array is already sorted, Bubble Sort has a time complexity of O(n), 
     because the algorithm only needs to make one pass through the array to verify that it is sorted and no swaps are needed.
 
@@ -24,3 +24,40 @@
     This is because the algorithm only needs to store a few temporary variables to perform the swaps, 
     and does not create any new data structures or arrays.
 */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// bubble sort function takes an arr and its length
+void bubbleSort(vector<int> &arr, int& n) {
+    // keep track of whether any elements were swapped 
+    bool swapped;
+    // go through every element in array
+    for (int i = 0; i < n; i++) {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        // if no two elements were swapped by inner loop then break
+        if (swapped == false) break;
+    }
+};
+
+int main() {
+    // initialize array
+    vector<int> arr = {52, 23, 3, 64, 1, 8, 121};
+    // initialize array size
+    int n = arr.size();
+    // apply bubble sort algorithm
+    bubbleSort(arr, n);
+
+    for (int i = 0; i < n; i++) {
+      cout << arr[i] << endl;
+	}
+  return 0;
+}
