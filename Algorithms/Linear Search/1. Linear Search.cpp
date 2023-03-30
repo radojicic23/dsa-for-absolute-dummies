0a0ce605311294cf9c9da0bc9dfaa4e09b99f338
Aleksa <fankerman11@gmail.com>
2023-03-30 18:09:27 +0200

    Added Time and Space complexity explanation for Linear Search and exercises.

--- Algorithms/Linear Search/1. Linear Search.cpp
+++ Algorithms/Linear Search/1. Linear Search.cpp
@@ -12,12 +12,25 @@
 /*
 - Linear search, also known as sequential search, is a simple algorithm for finding a value in a list or array. 
   The algorithm checks each element of the list in turn, starting from the first, until it finds the target value or reaches the end of the list.
+
+- TIME AND SPACE COMPLEXITY:
+
+    - Time Complexity:
+        - In the worst case, the linear search algorithm will have to check every element in the array, giving it a worst-case time complexity of O(n), 
+          where n is the size of the array. In the best case, the target element will be found at the beginning of the array, resulting in a time complexity of O(1). 
+          On average, the linear search algorithm has a time complexity of O(n/2) or O(n).
+
+    - Space Complexity:
+        - The space complexity of the linear search algorithm is O(1), because it uses a constant amount of additional space, regardless of the size of the input array. 
+          The only additional space used by the algorithm is the memory required to store the loop index variable i and the function arguments arr, n, and x. 
+          Therefore, the space used by the algorithm is independent of the size of the input array, making it a space-efficient algorithm
 */
 
 #include <iostream>
 #include <vector>
 
 using namespace std;
+
 
 int linearSearch(vector<int> &arr, int n, int target) {
     // go through arr
@@ -50,3 +63,10 @@ int main() {
 
     return 0;
 }
+
+
+/*
+- LeetCode Exercises: 
+    - Find Target Indices After Sorting Array: https://leetcode.com/problems/find-target-indices-after-sorting-array/
+    - Find Numbers with Even Number of Digits: https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+*/
