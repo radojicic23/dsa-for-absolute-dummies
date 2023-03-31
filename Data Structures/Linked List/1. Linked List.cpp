ebc7bfdaf10a1481199af9abdca5648e753b3a7b
Aleksa <fankerman11@gmail.com>
2023-03-31 14:09:29 +0200

    Added code explanation in C++ for Linked List.

--- Data Structures/Linked List/1. Linked List.cpp
+++ Data Structures/Linked List/1. Linked List.cpp
@@ -32,3 +32,130 @@
     - Delete (beginning) - O(1) - Only change the head pointer.
     - Delete (end) - O(n) - Move from the head to the node just before the item you want to delete.
 */
+
+#include <iostream>
+
+using namespace std;
+
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+
+class LinkedList {
+private:
+    Node* head;
+
+public:
+    LinkedList() {
+        this->head = NULL;
+    }
+
+    void insert(int data) {
+        Node* newNode = new Node(data);
+
+        if (this->head == NULL) {
+            this->head = newNode;
+        } else {
+            Node* current = this->head;
+            
+            while (current->next != NULL) {
+                current = current->next;
+            }
+            current->next = newNode;
+        }
+    }
+
+    void insertAtIndex(int data, int position) {
+        Node* newNode = new Node(data);
+
+        if (this->head == NULL || position == 0) {
+            newNode->next = this->head;
+            this->head = newNode;
+        } else {
+            Node* current = this->head;
+            int i = 0;
+
+            while (i < position - 1 && current->next != NULL) {
+                current = current->next;
+                i++;
+            }
+            newNode->next = current->next;
+            current->next = newNode;
+        }
+    }
+
+    void remove(int target) {
+        if (this->head == NULL) {
+            return;
+        }
+
+        if (this->head->data == target) {
+            Node* temp = this->head;
+            this->head = this->head->next;
+            delete temp;
+            return;
+        }
+
+        Node* current = this->head;
+
+        while (current->next != NULL) {
+            if (current->next->data == target) {
+                Node* temp = current->next;
+                current->next = current->next->next;
+                delete temp;
+                return;
+            }
+            current = current->next;
+        }
+    }
+
+    void print() {
+        Node* current = this->head;
+
+        while (current != NULL) {
+            cout << current->data << "-->";
+            current = current->next;
+        }
+        cout << endl;
+    }
+};
+
+
+int main() {
+    // initialize Linked List
+    LinkedList list;
+
+    // insert at the end of Linked List
+    list.insert(1);
+    list.insert(2);
+    list.insert(3);
+
+    // print whole Linked List
+    list.print();
+
+    // insert 4 at position 1
+    list.insertAtIndex(4, 1);
+
+    // print whole Linked List
+    list.print();
+
+    // insert 5 at position 0
+    list.insertAtIndex(5, 0);
+
+    list.print();
+
+    // remove 5 and 4
+    list.remove(5);
+    list.remove(4);
+
+    return 0;
+}
