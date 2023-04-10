fde89d0437ae71430a281496a67ce1f8825650db
Aleksa <fankerman11@gmail.com>
2023-04-10 15:50:08 +0200

    Updating structure of Two Pointers.

--- Algorithms/Two Pointers/1. Two Pointers.cpp
+++ Algorithms/Two Pointers/1. Two Pointers.cpp
@@ -15,6 +15,7 @@
   through the array in different directions to find the solution.
 
 - Time and Space Complexity: 
+
   In general, the time complexity of the Two Pointers algorithm is O(n), where n is the size of the input array or string. 
   This is because the algorithm only requires a single pass through the input, incrementing or decrementing the pointers as needed,
   and does not require nested loops or other expensive operations.
@@ -25,6 +26,30 @@
 
   However, it's important to note that the time and space complexity of the Two Pointers algorithm can be affected by the specifics of the problem being solved. 
   For example, if the input data is not already sorted, sorting it can take O(nlogn) time, which would increase the overall time complexity of the algorithm.
+*/
+
+/*
+- When to use Two Pointers Algorithm: 
+
+  1. When you need to find a subarray or substring that meets certain conditions, 
+     such as a subarray that adds up to a given sum or a substring that contains all the elements of a given set.
+
+  2. When you need to search for a target value in a sorted array or string, and you can't use binary search because the elements are not unique.
+
+  3. When you need to merge two sorted arrays or strings into a single sorted array or string.
+  
+  4. When you need to solve a problem that involves comparing two or more elements of an array or string, 
+     such as finding the longest palindromic substring or the maximum sum of non-overlapping intervals.
+
+  5. When you need to identify a pattern or sequence in an array or string, 
+     such as identifying all the pairs of elements that add up to a given value or all the triplets that satisfy a certain condition.
+
+- In general, the Two Pointers algorithm is most useful when the input data is sorted or partially sorted, 
+  since this allows the algorithm to efficiently navigate the data using two pointers. 
+  However, even if the data is not sorted, the Two Pointers algorithm can still be useful in certain situations, 
+  such as when you need to find a subarray that adds up to a given sum or a substring that contains all the elements of a given set.
+
+- Overall, the Two Pointers algorithm is a versatile and powerful technique that can be used to solve a wide variety of problems efficiently and effectively.
 */
 
 /*
@@ -64,29 +89,6 @@ int main() {
     }
     return 0;
 };
-
-/*
-- When to use Two Pointers Algorithm: 
-  1. When you need to find a subarray or substring that meets certain conditions, 
-     such as a subarray that adds up to a given sum or a substring that contains all the elements of a given set.
-
-  2. When you need to search for a target value in a sorted array or string, and you can't use binary search because the elements are not unique.
-
-  3. When you need to merge two sorted arrays or strings into a single sorted array or string.
-  
-  4. When you need to solve a problem that involves comparing two or more elements of an array or string, 
-     such as finding the longest palindromic substring or the maximum sum of non-overlapping intervals.
-
-  5. When you need to identify a pattern or sequence in an array or string, 
-     such as identifying all the pairs of elements that add up to a given value or all the triplets that satisfy a certain condition.
-
-- In general, the Two Pointers algorithm is most useful when the input data is sorted or partially sorted, 
-  since this allows the algorithm to efficiently navigate the data using two pointers. 
-  However, even if the data is not sorted, the Two Pointers algorithm can still be useful in certain situations, 
-  such as when you need to find a subarray that adds up to a given sum or a substring that contains all the elements of a given set.
-
-- Overall, the Two Pointers algorithm is a versatile and powerful technique that can be used to solve a wide variety of problems efficiently and effectively.
-*/
 
 /*
 - More Examples on LeetCode: 
