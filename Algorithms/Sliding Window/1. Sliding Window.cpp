e2f89dfd37cc601093df853a6812133456180120
Aleksa <fankerman11@gmail.com>
2023-04-12 18:39:05 +0200

    Added Time and Space complexity of Sliding Window algorithm.

--- Algorithms/Sliding Window/1. Sliding Window.cpp
+++ Algorithms/Sliding Window/1. Sliding Window.cpp
@@ -13,7 +13,7 @@
 */
 
 /*
-EXPLANATION:
+- EXPLANATION:
     - Sliding window algorithm is a commonly used technique in computer science that is used to solve a variety of problems 
       related to arrays, strings, and other data structures. It involves creating a window of fixed size 
       and sliding it over an array or string to perform some computation.
@@ -23,7 +23,7 @@ EXPLANATION:
       finding the maximum/minimum value in a set of data, or counting the number of occurrences of a specific element.
 
 
-WHEN TO USE SLIDING WINDOW:
+- WHEN TO USE SLIDING WINDOW:
     - Finding the maximum/minimum value in a set of data.
 
     - Counting the number of occurrences of a specific element.
@@ -31,4 +31,23 @@ WHEN TO USE SLIDING WINDOW:
     - Finding the longest substring without repeating characters.
 
     - Finding the maximum sum of a sub-array of size "X".
+*/
+
+/*
+- TIME AND SPACE COMPLEXITY:
+    - The time and space complexity of the sliding window algorithm depend on the specific problem being solved 
+      and the implementation of the algorithm.
+      Overall, the sliding window algorithm is often an efficient way to solve problems that involve subarrays or substrings of a given size.
+
+    - Time complexity:
+        - In general, the time complexity of the sliding window algorithm is O(n), where n is the size of the input array or string. 
+          This is because the algorithm involves iterating over the entire input once to create the initial window 
+          and then sliding the window once for each element of the input.
+
+    - Space complexity:
+        - The space complexity of the sliding window algorithm also depends on the specific problem and implementation.
+          In general, it's O(1). However, some problems might require additional data structures to be used within the window, 
+          such as a hash table or priority queue, which would increase the space complexity.
+
+
 */
