b62c0e0afe7468355931c1726f470b479765781f
Aleksa <fankerman11@gmail.com>
2023-04-14 12:18:20 +0200

    Updated gui and added explanation on when to use Bubble Sort.

--- Algorithms/Sorting/1. Bubble Sort.cpp
+++ Algorithms/Sorting/1. Bubble Sort.cpp
@@ -10,11 +10,12 @@
 */
 
 /*
-- Bubble Sort is a simple sorting algorithm that works by repeatedly swapping adjacent elements if they are in the wrong order. 
-  It gets its name because smaller elements "bubble" to the top of the array with each iteration.
+- EXPLANATION:
+  - Bubble Sort is a simple sorting algorithm that works by repeatedly swapping adjacent elements if they are in the wrong order. 
+    It gets its name because smaller elements "bubble" to the top of the array with each iteration.
 
-- Time and Space Complexity: 
-  Bubble Sort has a worst-case and average-case time complexity of O(n^2), where n is the number of elements in the array. 
+- TIME AND SPACE COMPLEXITY: 
+  - Bubble Sort has a worst-case and average-case time complexity of O(n^2), where n is the number of elements in the array. 
 
   - Best-case time complexity: In the best case, where the input array is already sorted, Bubble Sort has a time complexity of O(n), 
     because the algorithm only needs to make one pass through the array to verify that it is sorted and no swaps are needed.
@@ -23,6 +24,12 @@
     meaning that it sorts the array in place and does not require any additional space proportional to the size of the input. 
     This is because the algorithm only needs to store a few temporary variables to perform the swaps, 
     and does not create any new data structures or arrays.
+
+- WHEN TO USE BUBBLE SORT:
+  - Bubble sort is generally used for educational purposes or for sorting small datasets where efficiency is not a primary concern. 
+    It is also used as a simple example to demonstrate sorting algorithms and their complexity.
+
+  - In general, it is not recommended to use bubble sort for practical applications because it's not efficient.
 */
 
 #include <iostream>
