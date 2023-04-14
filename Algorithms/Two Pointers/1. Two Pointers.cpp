e1b155c3de7bc9a0995f0795c886573dd4df4169
Aleksa <fankerman11@gmail.com>
2023-04-14 12:33:50 +0200

    Restructuring explanations.

--- Algorithms/Two Pointers/1. Two Pointers.cpp
+++ Algorithms/Two Pointers/1. Two Pointers.cpp
@@ -10,26 +10,27 @@
 */
 
 /*
-- Certainly, the Two Pointers algorithm is a technique commonly used in array/string problems where you have to find a subsequence 
-  or a subset that satisfies a certain condition. It is called "Two Pointers" because the algorithm uses two pointers that move 
-  through the array in different directions to find the solution.
+- EXPLANATION:
+  - Certainly, the Two Pointers algorithm is a technique commonly used in array/string problems where you have to find a subsequence 
+    or a subset that satisfies a certain condition. It is called "Two Pointers" because the algorithm uses two pointers that move 
+    through the array in different directions to find the solution.
 
-- Time and Space Complexity: 
+- TIME AND SPACE COMPLEXITY: 
 
-  In general, the time complexity of the Two Pointers algorithm is O(n), where n is the size of the input array or string. 
-  This is because the algorithm only requires a single pass through the input, incrementing or decrementing the pointers as needed,
-  and does not require nested loops or other expensive operations.
+  - In general, the time complexity of the Two Pointers algorithm is O(n), where n is the size of the input array or string. 
+    This is because the algorithm only requires a single pass through the input, incrementing or decrementing the pointers as needed,
+    and does not require nested loops or other expensive operations.
 
-  The space complexity of the Two Pointers algorithm is also O(1), 
-  since the algorithm only requires a constant amount of extra space to store the pointers and any other variables used in the algorithm. 
-  This is because the algorithm modifies the input array or string in place, rather than creating new data structures to store intermediate results.
+  - The space complexity of the Two Pointers algorithm is also O(1), 
+    since the algorithm only requires a constant amount of extra space to store the pointers and any other variables used in the algorithm. 
+    This is because the algorithm modifies the input array or string in place, rather than creating new data structures to store intermediate results.
 
-  However, it's important to note that the time and space complexity of the Two Pointers algorithm can be affected by the specifics of the problem being solved. 
-  For example, if the input data is not already sorted, sorting it can take O(nlogn) time, which would increase the overall time complexity of the algorithm.
+  - However, it's important to note that the time and space complexity of the Two Pointers algorithm can be affected by the specifics of the problem being solved. 
+    For example, if the input data is not already sorted, sorting it can take O(nlogn) time, which would increase the overall time complexity of the algorithm.
 */
 
 /*
-- When to use Two Pointers Algorithm: 
+- WHEN TO USE TWO POINTERS: 
 
   1. When you need to find a subarray or substring that meets certain conditions, 
      such as a subarray that adds up to a given sum or a substring that contains all the elements of a given set.
