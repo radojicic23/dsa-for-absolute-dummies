ed64495ea45183709e70ec8721a398fb3724c4de
Aleksa <fankerman11@gmail.com>
2023-04-16 15:24:32 +0200

    Refactoring/Updating explanation for Kadane's algorithm.

--- Algorithms/Kadane's Algorithm/1. Kadane's Algorithm.cpp
+++ Algorithms/Kadane's Algorithm/1. Kadane's Algorithm.cpp
@@ -22,20 +22,21 @@
 */
 
 /*
-- Kadane's algorithm is a well-known algorithm for solving the maximum subarray problem, 
-  which asks for finding the contiguous subarray within array of numbers that has the largest sum.
+- EXPLANATION:
+  - Kadane's algorithm is a well-known algorithm for solving the maximum subarray problem, 
+    which asks for finding the contiguous subarray within array of numbers that has the largest sum.
 
-- The basic idea behind the algorithm is to iterate through the array, 
-  keeping track of the maximum sum seen so far and the current sum, and updating the maximum sum whenever a new maximum is found.
+  - The basic idea behind the algorithm is to iterate through the array, 
+    keeping track of the maximum sum seen so far and the current sum, and updating the maximum sum whenever a new maximum is found.
 
-- Steps:
+- STEPS:
     - Initialize variables to keep track of the current sum and maximum sum, setting them both to the first element of the array.
     - Starting from the second element, iterate through the rest of the array.
     - At each element, calculate the current sum by adding the current element to the previous current sum. 
     - Compare the current sum to the maximum sum and update the maximum sum if the current sum is greater.
     - Return the maximum sum as the result of the algorithm.
 
-Time and Space Complexity:
+- TIME AND SPACE COMPLEXITY:
 
     - The time complexity of Kadane's algorithm is O(n), where n is the length of the input array. 
       This is because the algorithm only loops through the array once.
@@ -43,6 +44,10 @@ Time and Space Complexity:
     - The space complexity of the algorithm is O(1), which means it uses a constant amount of extra space.
       This is because the algorithm only uses two variables (variable for current sum and varable for maximum sum in the end).
       Therefore, the space used by the algorithm does not depend on the size of the input array.
+
+- WHEN TO USE KADANE'S ALGORITHM:
+    - Kadane's algorithm is a good choice when there is a need to find the maximum subarray sum in an array of integers or 
+      to find the largest continuous subarray sum in an array that may contain both positive and negative integers.
 */
 
 #include <iostream>
