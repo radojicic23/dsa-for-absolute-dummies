9c1e35a555dfc2c89f90c13519cb1c5c93f4243b
Aleksa <fankerman11@gmail.com>
2023-04-18 16:55:48 +0200

    Added explanation for Array Data Structure in Python.

---
+++ Data Structures/Arrays/3. Array.py
@@ -0,0 +1,39 @@
+'''
+  /$$$$$$                                                  
+ /$$__  $$                                                 
+| $$  \ $$  /$$$$$$   /$$$$$$  /$$$$$$  /$$   /$$  /$$$$$$$
+| $$$$$$$$ /$$__  $$ /$$__  $$|____  $$| $$  | $$ /$$_____/
+| $$__  $$| $$  \__/| $$  \__/ /$$$$$$$| $$  | $$|  $$$$$$ 
+| $$  | $$| $$      | $$      /$$__  $$| $$  | $$ \____  $$
+| $$  | $$| $$      | $$     |  $$$$$$$|  $$$$$$$ /$$$$$$$/
+|__/  |__/|__/      |__/      \_______/ \____  $$|_______/ 
+                                        /$$  | $$          
+                                       |  $$$$$$/          
+                                        \______/ 
+'''
+
+'''
+- EXPLANATIONS:
+    - An array is a data structure that stores a collection of elements of the same data type in a contiguous block of memory. 
+      It provides a way to store and access a group of related values using a single variable name.
+      
+    - Each element in an array is identified by an index, which represents its position in the array. 
+      The index starts at 0 for the first element, and increases by 1 for each subsequent element. 
+      This means that if an array has n elements, the indices range from 0 to n-1.
+      
+    - Arrays can be used to store and manipulate different types of data, such as numbers, characters, strings, and objects. 
+      They are commonly used in programming for a variety of tasks, such as sorting, searching, and processing large amounts of data.
+      
+-  MOST COMMONLY USED BUILT-IN FUNCTIONS:
+
+    - array.append(x) - This function is used to add an element x to the end of the array.
+    - array.extend(iterable) - This function is used to add the elements of an iterable (such as a list or tuple) to the end of the array.
+    - array.insert(i, x) - This function is used to insert an element x at a specific index i in the array.
+    - array.remove(x) - This function is used to remove the first occurrence of element x from the array.
+    - array.pop(i) - This function is used to remove and return the element at a specific index i in the array. If no index is specified, the last element of the array is removed and returned.
+    - array.index(x) - This function is used to find the first occurrence of element x in the array and return its index.
+    - array.count(x) - This function is used to count the number of occurrences of element x in the array.
+    - array.sort() - This function is used to sort the elements of the array in ascending order.
+    - array.reverse() - This function is used to reverse the order of the elements in the array.
+    - len(array) - This function is used to return the number of elements in the array.
+'''
