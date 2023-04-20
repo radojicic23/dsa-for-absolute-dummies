88a570202f1cdfdc687f21531712ab718a8ef92c
Aleksa <fankerman11@gmail.com>
2023-04-20 18:50:04 +0200

    Added explanation through steps for Binary Search Algorithm in Python.

--- Algorithms/Binary Search/2. Binary Search.py
+++ Algorithms/Binary Search/2. Binary Search.py
@@ -14,7 +14,16 @@
 
 '''
 - EXPLANATION:
+
     - The Binary Search Algorithm is an efficient way to search for a specific value within a sorted list of values. 
       It works by repeatedly dividing the list in half and checking whether the target value is in the lower or upper half. 
       This process is repeated until the target value is found, or until it is determined that the target value is not in the list.
+      
+- STEPS:
+
+    - The search begins by comparing the target value to the middle element of the sorted array. 
+    - If the target value is equal to the middle element, the search is complete. 
+    - If the target value is less than the middle element, the search continues on the lower half of the array. 
+    - If the target value is greater than the middle element, the search continues on the upper half of the array. 
+    - This process continues until the target value is found or the search interval is empty.
 '''
