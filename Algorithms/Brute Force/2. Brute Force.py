e65d55f4dd4cdb99c10a667f0b1fe917e21b193d
Aleksa <fankerman11@gmail.com>
2023-04-22 13:15:13 +0200

    Added time and space complexity for Brute Force in Python.

--- Algorithms/Brute Force/2. Brute Force.py
+++ Algorithms/Brute Force/2. Brute Force.py
@@ -14,4 +14,15 @@
     - Brute Force algorithm is a straightforward approach to solve a problem by trying all possible solutions and selecting the best one. 
       This method is usually used when the input size is relatively small and has a limited number of possibilities. 
       Brute Force algorithm is also known as Exhaustive Search algorithm, as it exhausts all possible solutions to find the optimal solution.
+      
+- TIME AND SPACE COMPLEXITY:
+    - The time and space complexity of the Brute Force algorithm depends on the size of the input and the number of possible solutions.
+    
+    - For example, if we have an array of size n, and we want to find the maximum element in the array, 
+      the time complexity of the Brute Force algorithm is O(n), 
+      since we need to iterate through all n elements of the array to find the maximum element. 
+      The space complexity is O(1), since we only need to store one variable to keep track of the maximum value.
+    
+    - However, for some problems, the number of possible solutions can be very large, and the Brute Force algorithm may take exponential time. 
+      In such cases, the time complexity of the Brute Force algorithm can be expressed as O(2^n) or O(n!), where n is the size of the input.
 """
