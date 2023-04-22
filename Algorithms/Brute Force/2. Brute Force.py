d1d425a22cdbb2f84d882795e56a083019073ee8
Aleksa <fankerman11@gmail.com>
2023-04-22 13:12:45 +0200

    Added explanation for Brute Force algorithm in Python.

---
+++ Algorithms/Brute Force/2. Brute Force.py
@@ -0,0 +1,17 @@
+"""
+ /$$$$$$$                        /$$                     /$$$$$$$$                                     
+| $$__  $$                      | $$                    | $$_____/                                     
+| $$  \ $$  /$$$$$$  /$$   /$$ /$$$$$$    /$$$$$$       | $$     /$$$$$$   /$$$$$$   /$$$$$$$  /$$$$$$ 
+| $$$$$$$  /$$__  $$| $$  | $$|_  $$_/   /$$__  $$      | $$$$$ /$$__  $$ /$$__  $$ /$$_____/ /$$__  $$
+| $$__  $$| $$  \__/| $$  | $$  | $$    | $$$$$$$$      | $$__/| $$  \ $$| $$  \__/| $$      | $$$$$$$$
+| $$  \ $$| $$      | $$  | $$  | $$ /$$| $$_____/      | $$   | $$  | $$| $$      | $$      | $$_____/
+| $$$$$$$/| $$      |  $$$$$$/  |  $$$$/|  $$$$$$$      | $$   |  $$$$$$/| $$      |  $$$$$$$|  $$$$$$$
+|_______/ |__/       \______/    \___/   \_______/      |__/    \______/ |__/       \_______/ \_______/
+"""
+
+"""
+- EXPLANATION:
+    - Brute Force algorithm is a straightforward approach to solve a problem by trying all possible solutions and selecting the best one. 
+      This method is usually used when the input size is relatively small and has a limited number of possibilities. 
+      Brute Force algorithm is also known as Exhaustive Search algorithm, as it exhausts all possible solutions to find the optimal solution.
+"""
