eddf29352afa4a9e02ab8d325d9eeb2538f3edfc
Aleksa <fankerman11@gmail.com>
2023-04-22 13:40:08 +0200

    Added explanation on when to use Brute Force algorithm.

--- Algorithms/Brute Force/2. Brute Force.py
+++ Algorithms/Brute Force/2. Brute Force.py
@@ -25,4 +25,11 @@
     
     - However, for some problems, the number of possible solutions can be very large, and the Brute Force algorithm may take exponential time. 
       In such cases, the time complexity of the Brute Force algorithm can be expressed as O(2^n) or O(n!), where n is the size of the input.
+      
+- WHEN TO USE:
+    - The input size is small
+    - The problem space is not too large
+    - There is no obvious efficient algorithm available
+    - The problem requires exhaustive search
+    - Brute Force algorithm can be used as a baseline for other more advanced algorithms.
 """
