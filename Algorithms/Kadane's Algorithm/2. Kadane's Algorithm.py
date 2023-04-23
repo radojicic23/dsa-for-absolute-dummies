8299af722143bf86beafe43b0aed8f9621090e88
Aleksa <fankerman11@gmail.com>
2023-04-23 15:42:10 +0200

    Added steps for Kadane's algorithm.

--- Algorithms/Kadane's Algorithm/2. Kadane's Algorithm.py
+++ Algorithms/Kadane's Algorithm/2. Kadane's Algorithm.py
@@ -28,4 +28,11 @@
     
     - The basic idea behind the algorithm is to iterate through the array, 
       keeping track of the maximum sum seen so far and the current sum, and updating the maximum sum whenever a new maximum is found.
+      
+- STEPS:
+    - Initialize variables to keep track of the current sum and maximum sum, setting them both to the first element of the array.
+    - Starting from the second element, iterate through the rest of the array.
+    - At each element, calculate the current sum by adding the current element to the previous current sum. 
+    - Compare the current sum to the maximum sum and update the maximum sum if the current sum is greater.
+    - Return the maximum sum as the result of the algorithm.
 '''
