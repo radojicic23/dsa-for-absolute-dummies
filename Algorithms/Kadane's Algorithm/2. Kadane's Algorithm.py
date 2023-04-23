4d9c753379ddb5eaf2ab49312819cad0639b0528
Aleksa <fankerman11@gmail.com>
2023-04-23 15:43:56 +0200

    Added Time and Space complexity for Kadane's algorithm in Python.

--- Algorithms/Kadane's Algorithm/2. Kadane's Algorithm.py
+++ Algorithms/Kadane's Algorithm/2. Kadane's Algorithm.py
@@ -35,4 +35,13 @@
     - At each element, calculate the current sum by adding the current element to the previous current sum. 
     - Compare the current sum to the maximum sum and update the maximum sum if the current sum is greater.
     - Return the maximum sum as the result of the algorithm.
+
+- TIME AND SPACE COMPLEXITY:
+
+    - The time complexity of Kadane's algorithm is O(n), where n is the length of the input array. 
+      This is because the algorithm only loops through the array once.
+
+    - The space complexity of the algorithm is O(1), which means it uses a constant amount of extra space.
+      This is because the algorithm only uses two variables (variable for current sum and varable for maximum sum in the end).
+      Therefore, the space used by the algorithm does not depend on the size of the input array.
 '''
