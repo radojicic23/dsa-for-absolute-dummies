6be1fd7f3d7f33726ab0ca9296b29fc4e7bcb03d
Aleksa <fankerman11@gmail.com>
2023-04-26 18:32:51 +0200

    Added explanation on when to use Two Pointers (Python).

--- Algorithms/Two Pointers/2. Two Pointers.py
+++ Algorithms/Two Pointers/2. Two Pointers.py
@@ -31,4 +31,26 @@
       can be affected by the specifics of the problem being solved. 
       For example, if the input data is not already sorted, sorting it can take O(nlogn) time, 
       which would increase the overall time complexity of the algorithm.
+      
+- WHEN TO USE:
+
+    - When you need to find a subarray or substring that meets certain conditions, 
+      such as a subarray that adds up to a given sum or a substring that contains all the elements of a given set.
+
+    - When you need to search for a target value in a sorted array or string, 
+      and you can't use binary search because the elements are not unique.
+
+    - When you need to merge two sorted arrays or strings into a single sorted array or string.
+  
+    - When you need to solve a problem that involves comparing two or more elements of an array or string, 
+      such as finding the longest palindromic substring or the maximum sum of non-overlapping intervals.
+
+    - When you need to identify a pattern or sequence in an array or string, 
+      such as identifying all the pairs of elements that add up to a given value or all the triplets that satisfy a certain condition.
+
+
+- In general, the Two Pointers algorithm is most useful when the input data is sorted or partially sorted, 
+  since this allows the algorithm to efficiently navigate the data using two pointers. 
+  However, even if the data is not sorted, the Two Pointers algorithm can still be useful in certain situations, 
+  such as when you need to find a subarray that adds up to a given sum or a substring that contains all the elements of a given set.
 '''
