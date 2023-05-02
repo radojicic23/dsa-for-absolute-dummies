a5033e051b2a905033a3c90c884160fa86b9d8c3
Aleksa <fankerman11@gmail.com>
2023-05-02 12:29:08 +0200

    Added code explanation for Prefix Sum in Python.

--- Algorithms/Prefix Sum/2. Prefix Sum.py
+++ Algorithms/Prefix Sum/2. Prefix Sum.py
@@ -23,3 +23,28 @@
     - The space complexity of the algorithm is also O(N), since we need to allocate an additional vector of size N to store the prefix sum. 
       This is because the size of the prefix sum vector is equal to the size of the input array.
 '''
+
+# initialize prefix sum functon 
+# it takes array as an input
+# returns prefix sum
+def prefix_sum(array):
+    # initialize result empty list
+    # it will store the prefix sum 
+    result = []
+    # this varable keeps track of the cumulative sum
+    sum_so_far = 0
+    # iterate through array
+    for i in range(len(array)):
+        # update cumulative sum
+        sum_so_far += array[i]
+        # add sum so far to result list
+        result.append(sum_so_far)
+    # return prefix sum
+    return result
+
+# initialize new array
+new_array = [1, 2, 3, 4, 5, 6]
+# run prefix sum algorithm 
+result = prefix_sum(new_array)
+
+print(result) # [1, 3, 6, 10, 15, 21]
