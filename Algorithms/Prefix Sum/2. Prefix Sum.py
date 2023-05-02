957b99ab3f7bf8062d8dfc508ff5db0cc79963c5
Aleksa <fankerman11@gmail.com>
2023-05-02 12:11:19 +0200

    Added time and space complexity explanation for Prefix Sum in Python.

--- Algorithms/Prefix Sum/2. Prefix Sum.py
+++ Algorithms/Prefix Sum/2. Prefix Sum.py
@@ -14,4 +14,12 @@
     - The prefix sum algorithm, also known as the cumulative sum algorithm, 
       is a simple and efficient algorithm that calculates the cumulative sum of an array. 
       This algorithm is often used in computer science to preprocess an array in order to answer range queries in constant time.
+      
+- TIME AND SPACE COMPLEXITY:
+    - The time complexity of the prefix sum algorithm is O(N), where N is the size of the input array. 
+      This is because the algorithm iterates over each element of the array exactly once and performs a constant amount of work at each iteration. 
+      Therefore, the time required to compute the prefix sum is proportional to the size of the input array.
+    
+    - The space complexity of the algorithm is also O(N), since we need to allocate an additional vector of size N to store the prefix sum. 
+      This is because the size of the prefix sum vector is equal to the size of the input array.
 '''
