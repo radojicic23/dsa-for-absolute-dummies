1fec9d16e54087f47a3c4fd5606202eed02ee024
Aleksa <fankerman11@gmail.com>
2023-05-02 12:10:40 +0200

    Added explanation for Prefix Sum in Python.

---
+++ Algorithms/Prefix Sum/2. Prefix Sum.py
@@ -0,0 +1,17 @@
+'''
+ /$$$$$$$                      /$$$$$$  /$$                  /$$$$$$                         
+| $$__  $$                    /$$__  $$|__/                 /$$__  $$                        
+| $$  \ $$ /$$$$$$   /$$$$$$ | $$  \__/ /$$ /$$   /$$      | $$  \__/ /$$   /$$ /$$$$$$/$$$$ 
+| $$$$$$$//$$__  $$ /$$__  $$| $$$$    | $$|  $$ /$$/      |  $$$$$$ | $$  | $$| $$_  $$_  $$
+| $$____/| $$  \__/| $$$$$$$$| $$_/    | $$ \  $$$$/        \____  $$| $$  | $$| $$ \ $$ \ $$
+| $$     | $$      | $$_____/| $$      | $$  >$$  $$        /$$  \ $$| $$  | $$| $$ | $$ | $$
+| $$     | $$      |  $$$$$$$| $$      | $$ /$$/\  $$      |  $$$$$$/|  $$$$$$/| $$ | $$ | $$
+|__/     |__/       \_______/|__/      |__/|__/  \__/       \______/  \______/ |__/ |__/ |__/
+'''
+
+'''
+- EXPLANATION:
+    - The prefix sum algorithm, also known as the cumulative sum algorithm, 
+      is a simple and efficient algorithm that calculates the cumulative sum of an array. 
+      This algorithm is often used in computer science to preprocess an array in order to answer range queries in constant time.
+'''
