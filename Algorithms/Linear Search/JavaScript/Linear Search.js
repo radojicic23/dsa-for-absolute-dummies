9dee1a5d75592679bd3646c4f9d0f4c93e16af69
Aleksa <fankerman11@gmail.com>
2023-05-05 15:35:45 +0200

    Added explanation for time and space complexity in JavaScript.

--- Algorithms/Linear Search/JavaScript/Linear Search.js
+++ Algorithms/Linear Search/JavaScript/Linear Search.js
@@ -15,4 +15,18 @@
       The algorithm checks each element of the list in turn, starting from the first, 
       until it finds the target value or reaches the end of the list.
 
+
+- TIME AND SPACE COMPLEXITY:
+
+    - Time Complexity:
+
+        - In the worst case, the linear search algorithm will have to check every element in the array, 
+          giving it a worst-case time complexity of O(n), where n is the size of the array. 
+          In the best case, the target element will be found at the beginning of the array, resulting in a time complexity of O(1). 
+          On average, the linear search algorithm has a time complexity of O(n/2) or O(n).
+
+    - Space Complexity:
+    
+        - The algorithm only requires a constant amount of additional memory to store the loop index variable and the target value, 
+          so the space complexity of the Linear Search algorithm is O(1).
 */
