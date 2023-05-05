578ae63002a6f3609ff06b2bea1388c318790fb6
Aleksa <fankerman11@gmail.com>
2023-05-05 15:48:23 +0200

    Added code explanation in JavaScript for Linear Search.

--- Algorithms/Linear Search/JavaScript/Linear Search.js
+++ Algorithms/Linear Search/JavaScript/Linear Search.js
@@ -11,6 +11,7 @@
 
 /*
 - EXPLANATION:
+
     - Linear search, also known as sequential search, is a simple algorithm for finding a value in a list or array. 
       The algorithm checks each element of the list in turn, starting from the first, 
       until it finds the target value or reaches the end of the list.
@@ -43,3 +44,35 @@
     - Overall, linear search is a good choice when the dataset is small or unsorted, 
       when the data is stored in a linked list, or when it is used as a backup algorithm.
 */
+
+
+// linear search algorithm function 
+// it takes input array (list) in which the target value is to be searched
+// it takes target value that needs to be searched in the array (list)
+const linearSearch = (array, target) => {
+    // go through all numbers in the array
+    for (let i = 0; i < array.length; i++) {
+        // if the current item happens to be same as target
+        if (array[i] === target) {
+            // return index position of that value
+            return i;
+        }
+    }
+    // if target not found
+    return -1;
+}
+
+
+// initialize new array
+const newArray = [2, 0, 18, 32, 10, 1, 5];
+// initialize target value
+const targetValue = 18;
+// run linear search algorithm
+const result = linearSearch(newArray, targetValue);
+
+// check to see if the target is in the array
+if (result !== -1) {
+    console.log(`Target value ${targetValue} found at index ${result}.`);
+} else {
+    console.log(`Target value ${targetValue} not found.`);
+}
