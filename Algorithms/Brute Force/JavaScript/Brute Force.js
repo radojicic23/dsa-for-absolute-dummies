5d9d2058e65e07341cf5fedf6046914ceb2fe248
Aleksa <fankerman11@gmail.com>
2023-05-07 16:23:09 +0200

    [JavaScript] Added code explanation for Brute Force.

--- Algorithms/Brute Force/JavaScript/Brute Force.js
+++ Algorithms/Brute Force/JavaScript/Brute Force.js
@@ -39,3 +39,29 @@
     - The problem requires exhaustive search.
     - Brute Force algorithm can be used as a baseline for other more advanced algorithms.
 */
+
+
+// initialzie brute force algorithm function
+// it takes array as an input  
+const bruteForce = (array) => {
+    // we assume that the first value is the max value from the array
+    let maxValue = array[0]
+    // go through all elements in the array except first element
+    for (let i = 1; i < array.length; i++) {
+        // if the current value is greater than max value before
+        if (array[i] > maxValue) {
+            // update max value
+            maxValue = array[i];
+        }
+    }
+    // return max value
+    return maxValue;
+}
+
+
+// initialize new array
+const newArray = [20, 50, 2, 321, 8, 121, 256, 133, 32]
+// run brute force algorithm
+const maxValue = bruteForce(newArray);
+
+console.log(`Max value of this array is: ${maxValue}`); // 321
