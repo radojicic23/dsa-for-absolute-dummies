5630ebc9579b07844b642778aa4a4e362f91c769
Aleksa <fankerman11@gmail.com>
2023-05-07 16:15:59 +0200

    [JavaScript] Added explanation on when to use Brute Force.

--- Algorithms/Brute Force/JavaScript/Brute Force.js
+++ Algorithms/Brute Force/JavaScript/Brute Force.js
@@ -24,10 +24,18 @@
     - For example, if we have an array of size n, and we want to find the maximum element in the array, 
       the time complexity of the Brute Force algorithm is O(n), 
       since we need to iterate through all n elements of the array to find the maximum element. 
-      
+
       The space complexity is O(1), since we only need to store one variable to keep track of the maximum value.
     
     - However, for some problems, the number of possible solutions can be very large, and the Brute Force algorithm may take exponential time. 
       In such cases, the time complexity of the Brute Force algorithm can be expressed as O(2^n) or O(n!), where n is the size of the input.
 
+
+- WHEN TO USE:
+
+    - The input size is small.
+    - The problem space is not too large.
+    - There is no obvious efficient algorithm available.
+    - The problem requires exhaustive search.
+    - Brute Force algorithm can be used as a baseline for other more advanced algorithms.
 */
