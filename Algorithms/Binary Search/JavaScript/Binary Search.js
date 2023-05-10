b60217f49a00c79800f9d4038bb749583e396e91
Aleksa <fankerman11@gmail.com>
2023-05-10 19:34:03 +0200

    [JavaScript] Added explanation on when to use Binary Search.

--- Algorithms/Binary Search/JavaScript/Binary Search.js
+++ Algorithms/Binary Search/JavaScript/Binary Search.js
@@ -30,4 +30,24 @@
     - The space complexity of the Binary Search Algorithm is O(1), which means that it uses a constant amount of memory. 
       This is because the algorithm only needs to store a few variables (low, high, and mid) 
       to keep track of the search space and the current position. 
+
+
+- WHEN TO USE:
+
+    - Searching in a sorted array or list: If you have a sorted array or list, 
+      binary search algorithm can be used to search for a specific element in it.
+  
+    - Finding the maximum or minimum value in a function: 
+      If you have a function that is monotonically increasing or decreasing over a range, 
+      you can use binary search algorithm to find the maximum or minimum value in that range. 
+      For example, if you have a function that represents the height of a person at a given age, 
+      you can use binary search algorithm to find the age at which the person reaches their maximum height.
+  
+    - If you have a sorted array of integers, you can use binary search algorithm to find the first occurrence of a specific integer.
+  
+    - Finding the closest value to a target value: 
+      If you have a sorted array or list and you want to find the closest value to a target value, you can use binary search algorithm.
+
+- Overall, binary search algorithm is most useful when we are dealing with sorted arrays or lists, 
+  monotonic properties, and when we need to find the maximum or minimum value in a function.
 */
