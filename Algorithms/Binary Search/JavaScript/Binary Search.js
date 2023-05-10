8a1e38dc924d18bfc9c1d3b4e1d707a6d05b187b
Aleksa <fankerman11@gmail.com>
2023-05-10 19:31:14 +0200

    [JavaScript] Added explanation for Binary Search.

---
+++ Algorithms/Binary Search/JavaScript/Binary Search.js
@@ -0,0 +1,21 @@
+/*
+ /$$$$$$$  /$$                                                /$$$$$$                                          /$$      
+| $$__  $$|__/                                               /$$__  $$                                        | $$      
+| $$  \ $$ /$$ /$$$$$$$   /$$$$$$   /$$$$$$  /$$   /$$      | $$  \__/  /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$$| $$$$$$$ 
+| $$$$$$$ | $$| $$__  $$ |____  $$ /$$__  $$| $$  | $$      |  $$$$$$  /$$__  $$ |____  $$ /$$__  $$ /$$_____/| $$__  $$
+| $$__  $$| $$| $$  \ $$  /$$$$$$$| $$  \__/| $$  | $$       \____  $$| $$$$$$$$  /$$$$$$$| $$  \__/| $$      | $$  \ $$
+| $$  \ $$| $$| $$  | $$ /$$__  $$| $$      | $$  | $$       /$$  \ $$| $$_____/ /$$__  $$| $$      | $$      | $$  | $$
+| $$$$$$$/| $$| $$  | $$|  $$$$$$$| $$      |  $$$$$$$      |  $$$$$$/|  $$$$$$$|  $$$$$$$| $$      |  $$$$$$$| $$  | $$
+|_______/ |__/|__/  |__/ \_______/|__/       \____  $$       \______/  \_______/ \_______/|__/       \_______/|__/  |__/
+                                             /$$  | $$                                                                  
+                                            |  $$$$$$/                                                                  
+                                             \______/ 
+*/
+
+/*
+- EXPLANATION:
+
+    - The Binary Search Algorithm is an efficient way to search for a specific value within a sorted list of values. 
+      It works by repeatedly dividing the list in half and checking whether the target value is in the lower or upper half. 
+      This process is repeated until the target value is found, or until it is determined that the target value is not in the list.
+*/
