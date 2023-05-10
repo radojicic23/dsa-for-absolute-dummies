482bd8d61f5b8f1d1707e398aaae0cd3380396b4
Aleksa <fankerman11@gmail.com>
2023-05-10 19:32:18 +0200

    [JavaScript] Added time and space complexity for Binary Search.

--- Algorithms/Binary Search/JavaScript/Binary Search.js
+++ Algorithms/Binary Search/JavaScript/Binary Search.js
@@ -18,4 +18,16 @@
     - The Binary Search Algorithm is an efficient way to search for a specific value within a sorted list of values. 
       It works by repeatedly dividing the list in half and checking whether the target value is in the lower or upper half. 
       This process is repeated until the target value is found, or until it is determined that the target value is not in the list.
+
+
+- TIME AND SPACE COMPLEXITY:
+
+    - The time complexity of the Binary Search Algorithm is O(log n), where n is the number of elements in the list. 
+      This is because each iteration of the loop cuts the size of the search space in half. 
+      Therefore, the algorithm can find the target value in a large list much more quickly than a linear search 
+      that would have a time complexity of O(n).
+      
+    - The space complexity of the Binary Search Algorithm is O(1), which means that it uses a constant amount of memory. 
+      This is because the algorithm only needs to store a few variables (low, high, and mid) 
+      to keep track of the search space and the current position. 
 */
