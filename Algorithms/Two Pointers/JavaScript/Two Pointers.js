5e24277659f3ef45aa96260e07fa7c9a4e58c7cf
Aleksa <fankerman11@gmail.com>
2023-05-12 12:59:50 +0200

    [JavaScript] Added explanation for Two Pointers.

---
+++ Algorithms/Two Pointers/JavaScript/Two Pointers.js
@@ -0,0 +1,18 @@
+/*
+ /$$$$$$$$                            /$$$$$$$           /$$             /$$                                  
+|__  $$__/                           | $$__  $$         |__/            | $$                                  
+   | $$ /$$  /$$  /$$  /$$$$$$       | $$  \ $$ /$$$$$$  /$$ /$$$$$$$  /$$$$$$    /$$$$$$   /$$$$$$   /$$$$$$$
+   | $$| $$ | $$ | $$ /$$__  $$      | $$$$$$$//$$__  $$| $$| $$__  $$|_  $$_/   /$$__  $$ /$$__  $$ /$$_____/
+   | $$| $$ | $$ | $$| $$  \ $$      | $$____/| $$  \ $$| $$| $$  \ $$  | $$    | $$$$$$$$| $$  \__/|  $$$$$$ 
+   | $$| $$ | $$ | $$| $$  | $$      | $$     | $$  | $$| $$| $$  | $$  | $$ /$$| $$_____/| $$       \____  $$
+   | $$|  $$$$$/$$$$/|  $$$$$$/      | $$     |  $$$$$$/| $$| $$  | $$  |  $$$$/|  $$$$$$$| $$       /$$$$$$$/
+   |__/ \_____/\___/  \______/       |__/      \______/ |__/|__/  |__/   \___/   \_______/|__/      |_______/ 
+*/
+
+/*
+- EXPLANATION:
+
+    - Certainly, the Two Pointers algorithm is a technique commonly used in array/string problems where you have to find a subsequence 
+      or a subset that satisfies a certain condition. It is called "Two Pointers" because the algorithm uses two pointers that move 
+      through the array in different directions to find the solution.
+*/
