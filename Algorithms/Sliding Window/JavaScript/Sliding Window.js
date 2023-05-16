a67de40522c48b07b5de9be72041dec4b6e35d0c
Aleksa <fankerman11@gmail.com>
2023-05-16 18:30:10 +0200

    [JavaScript] Added explanation for Sliding Window algorithm.

---
+++ Algorithms/Sliding Window/JavaScript/Sliding Window.js
@@ -0,0 +1,25 @@
+/*
+  /$$$$$$  /$$ /$$       /$$ /$$                           /$$      /$$ /$$                 /$$                        
+ /$$__  $$| $$|__/      | $$|__/                          | $$  /$ | $$|__/                | $$                        
+| $$  \__/| $$ /$$  /$$$$$$$ /$$ /$$$$$$$   /$$$$$$       | $$ /$$$| $$ /$$ /$$$$$$$   /$$$$$$$  /$$$$$$  /$$  /$$  /$$
+|  $$$$$$ | $$| $$ /$$__  $$| $$| $$__  $$ /$$__  $$      | $$/$$ $$ $$| $$| $$__  $$ /$$__  $$ /$$__  $$| $$ | $$ | $$
+ \____  $$| $$| $$| $$  | $$| $$| $$  \ $$| $$  \ $$      | $$$$_  $$$$| $$| $$  \ $$| $$  | $$| $$  \ $$| $$ | $$ | $$
+ /$$  \ $$| $$| $$| $$  | $$| $$| $$  | $$| $$  | $$      | $$$/ \  $$$| $$| $$  | $$| $$  | $$| $$  | $$| $$ | $$ | $$
+|  $$$$$$/| $$| $$|  $$$$$$$| $$| $$  | $$|  $$$$$$$      | $$/   \  $$| $$| $$  | $$|  $$$$$$$|  $$$$$$/|  $$$$$/$$$$/
+ \______/ |__/|__/ \_______/|__/|__/  |__/ \____  $$      |__/     \__/|__/|__/  |__/ \_______/ \______/  \_____/\___/ 
+                                           /$$  \ $$                                                                   
+                                          |  $$$$$$/                                                                   
+                                           \______/  
+*/
+
+/*
+- EXPLANATION:
+
+    - Sliding window algorithm is a commonly used technique in computer science that is used to solve a variety of problems 
+      related to arrays, strings, and other data structures. It involves creating a window of fixed size 
+      and sliding it over an array or string to perform some computation.
+      
+    - The size of the window and the number of elements it moves at each step can be adjusted to suit the needs 
+      of the specific problem being solved. The technique is commonly used in algorithms that involve 
+      finding the maximum/minimum value in a set of data, or counting the number of occurrences of a specific element.
+*/
