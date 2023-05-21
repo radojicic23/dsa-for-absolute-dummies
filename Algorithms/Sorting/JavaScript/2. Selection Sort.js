d744252241e6886a5d1f1fe8384f87d9809603fb
Aleksa <fankerman11@gmail.com>
2023-05-21 22:53:44 +0200

    [JavaScript] Added time and space complexity explanation for Selection Sort.

--- Algorithms/Sorting/JavaScript/2. Selection Sort.js
+++ Algorithms/Sorting/JavaScript/2. Selection Sort.js
@@ -13,4 +13,16 @@
 - EXPLANATION:
     - Selection sort is a simple sorting algorithm that works by repeatedly 
       finding the minimum element from an unsorted portion of the array and putting it at the beginning of the array.
+
+
+- TIME AND SPACE COMPLEXITY:
+
+    - The Time Complexity of the selection sort algorithm is O(n^2), where "n" is the number of elements in the array,
+      which means it's not the most efficient sorting algorithm out there, but it's still useful for small arrays or for educational purposes.
+      This is because the algorithm has to compare each element with every other element in the array, 
+      which results in n*(n-1)/2 comparisons in the worst case.
+
+    - The Space Complexity of the selection sort algorithm is O(1), 
+      which means that it uses a constant amount of additional memory regardless of the size of the input array. 
+      This is because the algorithm sorts the elements in-place, without requiring any additional data structures such as arrays, lists, or trees.
 */
