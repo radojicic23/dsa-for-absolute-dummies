048025c8604695abae819e484e0317324d541c55
Aleksa <fankerman11@gmail.com>
2023-05-21 22:55:28 +0200

    [JavaScript] Added LeetCode examples for Selection Sort.

--- Algorithms/Sorting/JavaScript/2. Selection Sort.js
+++ Algorithms/Sorting/JavaScript/2. Selection Sort.js
@@ -32,3 +32,17 @@
     - In general, selection sort is a good choice when the dataset is small or nearly sorted, when memory space is limited, 
       or when minimizing the number of swaps is important.
 */
+
+
+
+
+
+
+
+
+
+/*
+- More Examples on LeetCode:
+
+  - Sort Colors: https://leetcode.com/problems/sort-colors/
+*/
