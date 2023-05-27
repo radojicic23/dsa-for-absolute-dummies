5943eb5360529252b8a8d71bc1b66f0f29f4d074
Aleksa <fankerman11@gmail.com>
2023-05-27 17:43:47 +0200

    [Python] Added time and space complexity explanation for Insertion Sort.

--- Algorithms/Sorting/Python/3. Insertion Sort.py
+++ Algorithms/Sorting/Python/3. Insertion Sort.py
@@ -19,4 +19,13 @@
     - If the element is smaller than the sorted elements to its left, move those elements one position to the right to make space for the new element.
     - Insert the new element into its correct position in the sorted portion of the array.
     - Repeat steps 3 and 4 for each subsequent element in the unsorted portion of the array until the entire array is sorted.
+    
+    
+- TIME AND SPACE COMPLEXITY:
+
+  - The time complexity of the insertion sort algorithm is O(n^2), where n is the number of elements in the array. 
+    This is because, in the worst case scenario, the algorithm must make n comparisons for each of the n elements in the array.
+
+  - The space complexity of the insertion sort algorithm is O(1), meaning it requires a constant amount of extra space. 
+    This is because the algorithm sorts the input array in place, without requiring any additional data structures or temporary storage.
 '''
