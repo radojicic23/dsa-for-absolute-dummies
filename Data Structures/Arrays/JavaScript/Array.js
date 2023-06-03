cd7916a4935ffc24b5799412df693b5ce6572872
Aleksa <fankerman11@gmail.com>
2023-06-03 13:44:59 +0200

    [JavaScript] Added explanation for Arrays.

---
+++ Data Structures/Arrays/JavaScript/Array.js
@@ -0,0 +1,26 @@
+/*
+  /$$$$$$                                                  
+ /$$__  $$                                                 
+| $$  \ $$  /$$$$$$   /$$$$$$  /$$$$$$  /$$   /$$  /$$$$$$$
+| $$$$$$$$ /$$__  $$ /$$__  $$|____  $$| $$  | $$ /$$_____/
+| $$__  $$| $$  \__/| $$  \__/ /$$$$$$$| $$  | $$|  $$$$$$ 
+| $$  | $$| $$      | $$      /$$__  $$| $$  | $$ \____  $$
+| $$  | $$| $$      | $$     |  $$$$$$$|  $$$$$$$ /$$$$$$$/
+|__/  |__/|__/      |__/      \_______/ \____  $$|_______/ 
+                                        /$$  | $$          
+                                       |  $$$$$$/          
+                                        \______/ 
+*/
+
+/*
+- EXPLANATIONS:
+    - An array is a data structure that stores a collection of elements of the same data type in a contiguous block of memory. 
+      It provides a way to store and access a group of related values using a single variable name.
+      
+    - Each element in an array is identified by an index, which represents its position in the array. 
+      The index starts at 0 for the first element, and increases by 1 for each subsequent element. 
+      This means that if an array has n elements, the indices range from 0 to n-1.
+      
+    - Arrays can be used to store and manipulate different types of data, such as numbers, characters, strings, and objects. 
+      They are commonly used in programming for a variety of tasks, such as sorting, searching, and processing large amounts of data.
+*/
