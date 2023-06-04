68e9bbdc41527a744e4733b7935a09bfe89c89e5
Aleksa <fankerman11@gmail.com>
2023-06-04 16:25:00 +0200

    [Python] Added explanation for HashMap.

---
+++ Data Structures/HashMap/Python/HashMap.py
@@ -0,0 +1,23 @@
+'''
+ /$$   /$$                     /$$       /$$      /$$                    
+| $$  | $$                    | $$      | $$$    /$$$                    
+| $$  | $$  /$$$$$$   /$$$$$$$| $$$$$$$ | $$$$  /$$$$  /$$$$$$   /$$$$$$ 
+| $$$$$$$$ |____  $$ /$$_____/| $$__  $$| $$ $$/$$ $$ |____  $$ /$$__  $$
+| $$__  $$  /$$$$$$$|  $$$$$$ | $$  \ $$| $$  $$$| $$  /$$$$$$$| $$  \ $$
+| $$  | $$ /$$__  $$ \____  $$| $$  | $$| $$\  $ | $$ /$$__  $$| $$  | $$
+| $$  | $$|  $$$$$$$ /$$$$$$$/| $$  | $$| $$ \/  | $$|  $$$$$$$| $$$$$$$/
+|__/  |__/ \_______/|_______/ |__/  |__/|__/     |__/ \_______/| $$____/ 
+                                                               | $$      
+                                                               | $$      
+                                                               |__/ 
+'''
+
+'''
+- EXPLANATIONS:
+    - In Python, the HashMap data structure is commonly referred to as a dictionary or dict. 
+      It is a built-in data type that allows you to store and retrieve key-value pairs efficiently. 
+      The HashMap provides fast access to values based on their corresponding keys.
+    
+
+
+'''
