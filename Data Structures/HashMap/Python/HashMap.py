6bea9d3425e84ce0fd3bc7be603f1c48d701c5d1
Aleksa <fankerman11@gmail.com>
2023-06-05 16:53:08 +0200

    [Python] Added brief explanation for HashMap.

--- Data Structures/HashMap/Python/HashMap.py
+++ Data Structures/HashMap/Python/HashMap.py
@@ -17,6 +17,25 @@
     - In Python, the HashMap data structure is commonly referred to as a dictionary or dict. 
       It is a built-in data type that allows you to store and retrieve key-value pairs efficiently. 
       The HashMap provides fast access to values based on their corresponding keys.
+      
+      
+- BRIEF EXPLANATION:
+
+    - Key-Value Mapping: A HashMap is a collection of key-value pairs. 
+      Each key in the HashMap must be unique, and it is associated with a corresponding value. 
+      The keys are used to look up and retrieve the associated values.
+      
+    - Mutable and Dynamic: HashMap in Python is mutable, which means you can modify, add, or remove key-value pairs after creating it. 
+      The size of the HashMap can dynamically change as you insert or remove elements.
+      
+    - Hashing: Behind the scenes, a HashMap uses a hash function to map keys to their corresponding positions in the underlying data structure. 
+      This hash function calculates a unique hash code for each key and determines where the key-value pair should be stored. 
+      The hash function enables efficient retrieval of values based on their keys.
+      
+    - Efficient Retrieval: The primary advantage of using a HashMap is its fast retrieval time complexity. 
+      It provides constant-time average case performance for accessing elements, 
+      meaning that the time taken to access a value is not dependent on the size of the HashMap. 
+      Retrieval is performed by providing the key, and the associated value is returned.
 '''
 
 # Creating a HashMap
@@ -40,8 +59,6 @@ print(my_dict.get("orange"))  # Output: None
 # Iterating over key-value pairs
 for key, value in my_dict.items():
     print(key, value)
-
-
 
 
 
