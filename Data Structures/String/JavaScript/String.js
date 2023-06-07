dbe06782f60c653a24814b8de91c7818ab8d6a56
Aleksa <fankerman11@gmail.com>
2023-06-07 18:00:51 +0200

    [JavaScript] Added explanation for Strings.

---
+++ Data Structures/String/JavaScript/String.js
@@ -0,0 +1,27 @@
+/*
+  /$$$$$$   /$$               /$$                    
+ /$$__  $$ | $$              |__/                    
+| $$  \__//$$$$$$    /$$$$$$  /$$ /$$$$$$$   /$$$$$$ 
+|  $$$$$$|_  $$_/   /$$__  $$| $$| $$__  $$ /$$__  $$
+ \____  $$ | $$    | $$  \__/| $$| $$  \ $$| $$  \ $$
+ /$$  \ $$ | $$ /$$| $$      | $$| $$  | $$| $$  | $$
+|  $$$$$$/ |  $$$$/| $$      | $$| $$  | $$|  $$$$$$$
+ \______/   \___/  |__/      |__/|__/  |__/ \____  $$
+                                            /$$  \ $$
+                                           |  $$$$$$/
+                                            \______/ 
+*/
+
+/*
+- EXPLANATION:
+
+    - In JavaScript, a string is a data type that represents a sequence of characters. 
+      It is used to store and manipulate textual data such as names, messages, and other pieces of information.
+      A string in JavaScript is created by enclosing the characters within single quotes (''), double quotes ("") or backticks (``).
+
+    - Strings are immutable in JavaScript, which means once a string is created, it cannot be changed. 
+      However, you can perform various operations on strings to extract information or manipulate them in different ways.
+
+
+
+*/
