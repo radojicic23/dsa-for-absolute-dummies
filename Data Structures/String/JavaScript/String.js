5ab544eddfb1d32b7693c5cd191f69f8ac252f08
Aleksa <fankerman11@gmail.com>
2023-06-07 18:04:22 +0200

    [JavaScript] Added most common operations for Strings.

--- Data Structures/String/JavaScript/String.js
+++ Data Structures/String/JavaScript/String.js
@@ -23,5 +23,10 @@
       However, you can perform various operations on strings to extract information or manipulate them in different ways.
 
 
-
+SOME COMMON STRING OPERATIONS IN JAVASCRIPT:
+    - Accessing characters: You can access individual characters in a string using square brackets and the index of the character (indexes start at 0).
+    - Concatenation: You can concatenate (join) two or more strings together using the + operator or the concat() method.
+    - String length: You can find the length of a string (number of characters) using the length property.
+    - String methods: JavaScript provides several built-in methods for working with strings. 
+      Some commonly used methods include toUpperCase(), toLowerCase(), slice(), substring(), indexOf(), replace(), and split().
 */
