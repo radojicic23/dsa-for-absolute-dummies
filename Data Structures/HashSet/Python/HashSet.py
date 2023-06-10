5df33b4af374672d3c194a2385a9eb19ef5d83a9
Aleksa <fankerman11@gmail.com>
2023-06-10 14:49:22 +0200

    [Python] Added explanation for HashSet.

---
+++ Data Structures/HashSet/Python/HashSet.py
@@ -0,0 +1,18 @@
+'''
+ /$$   /$$                     /$$        /$$$$$$              /$$    
+| $$  | $$                    | $$       /$$__  $$            | $$    
+| $$  | $$  /$$$$$$   /$$$$$$$| $$$$$$$ | $$  \__/  /$$$$$$  /$$$$$$  
+| $$$$$$$$ |____  $$ /$$_____/| $$__  $$|  $$$$$$  /$$__  $$|_  $$_/  
+| $$__  $$  /$$$$$$$|  $$$$$$ | $$  \ $$ \____  $$| $$$$$$$$  | $$    
+| $$  | $$ /$$__  $$ \____  $$| $$  | $$ /$$  \ $$| $$_____/  | $$ /$$
+| $$  | $$|  $$$$$$$ /$$$$$$$/| $$  | $$|  $$$$$$/|  $$$$$$$  |  $$$$/
+|__/  |__/ \_______/|_______/ |__/  |__/ \______/  \_______/   \___/ 
+'''
+
+'''
+- EXPLANATION:
+    - In Python, there is no built-in class named HashSet. 
+      However, Python provides a built-in class called set, which can be used to achieve similar functionality to a hash set. 
+      The set class in Python represents an unordered collection of unique elements. 
+      It is implemented using a hash table data structure, which allows for efficient membership testing, insertion, and deletion operations.
+'''
